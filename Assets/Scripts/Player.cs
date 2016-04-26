db882fa015a16aee3467c655e85372748dc1a7ff
ignac_000 <ignac_000@NOMIO>
2016-04-26 15:40:49 -0300

    Add project files.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Vector2 fuerzaSalto = new Vector2 (0, 300);
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp("space")){
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().AddForce(fuerzaSalto);
+
+        }
+	}
+
+}
